e3d20bb563ffeaf90a874e7281f4a8bdccfcbea0
Njenga Kabiru <njengakabiru@gmail.com>
2018-07-05 14:37:40 +0300

    Remove default boilerplate and add Buefy

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Buefy from "buefy";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.use(Buefy);
 
 new Vue({
   router,
